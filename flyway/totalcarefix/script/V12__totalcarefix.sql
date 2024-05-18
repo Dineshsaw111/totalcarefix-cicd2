7204f663b0b03bbe53ce81785a56a1d7cbf7bef7
Dineshsaw111 <159011678+Dineshsaw111@users.noreply.github.com>
2024-05-18 23:12:57 +0530

    Update V12__totalcarefix.sql

--- flyway/totalcarefix/script/V12__totalcarefix.sql
+++ flyway/totalcarefix/script/V12__totalcarefix.sql
@@ -209,7 +209,7 @@ INSERT INTO booking (booker_id, tech_id, status_id, address_id,skill_id, message
 -- Inserting into FEEDBACKS table
 INSERT INTO feedbacks (user_id,booking_id, tech_id, message, rating) VALUES
   (1, 2, 4, 'Great service!', 5),
-  (2, 1, 2, 'Very knowledgeable technician.', 4),
+  (2, 1, 2, 'Very knowledgeable technician.', 4);
 
 
 CREATE VIEW users_details_view AS
